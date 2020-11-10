76bef7dffa2c6288168aef54a7fa7aa39b6570e1
Hanns Holger Rutz <contact@sciss.de>
2020-11-10 10:23:09 +0100

    ...

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rsync -rltDuv target/scala-2.13/soundprocesses-js-test-opt.js* www.sciss.de@ssh.strato.de:temp/soundprocesses.js/target/scala-2.13/
+rsync -rltDuv index*.html www.sciss.de@ssh.strato.de:temp/soundprocesses.js/
